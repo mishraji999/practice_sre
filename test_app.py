954e17fe92a16391a40a0133295d116a47793a87
Piyush Mishra <piyushm@Piyushs-MacBook-Pro.local>
2025-12-29 13:20:04 +0530

    basic api test added

--- test_app.py
+++ test_app.py
@@ -1,9 +1,9 @@
 from fastapi.testclient import TestClient
 from main import app
+#httpx added
 
 client = TestClient(app)
 
 def test_health():
     response = client.get("/")
     assert response.status_code == 200
-    
